ed85f9c413055cede8667766ffe448cbdf30460e
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-03-30 21:26:07 -0300

    Entidade Cliente e tela de cadastro

---
+++ erp-management-system/src/main/java/com/luiz/erp/model/Cliente.java
@@ -0,0 +1,38 @@
+package com.luiz.erp.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "in_pf")
+    private Boolean inPf;
+
+    @Column(name = "nr_cpf")
+    private String nrCpf;
+
+    @Column(name = "nr_cnpj")
+    private String nrCnpj;
+
+    @Column(name = "tx_nome")
+    private String txNmCliente;
+
+    @Column(name = "tx_razao_social")
+    private String txNmRazaoSocial;
+
+    @Column(name = "sexo")
+    private String inSexoMasc;
+
+    // getters e setters
+}
